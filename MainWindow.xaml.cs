a69a14e536b8e5fa14724743d52002cd5caeb94e
clf <443055589@qq.com>
2019-09-09 16:39:57 +0800

    optimize

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HttpHelper;
 
 namespace HttpHelperDemo
 {
@@ -23,6 +25,101 @@ namespace HttpHelperDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            TestGet();
+        }
+
+        private void Generic_Click(object sender, RoutedEventArgs e)
+        {
+
+            testPost();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            testPost();
+           
+        }
+
+        public void testPost()
+        {
+            string url = "http://127.0.0.1:8080/nbm/login/test";
+            NameValueCollection collection = new NameValueCollection
+            {
+                {"name","code"},
+                {"age","80" },
+                {"salary","12.90" }
+            };
+            System.Net.WebHeaderCollection headerCollection = new System.Net.WebHeaderCollection
+           {
+               {"token","qmfamdfasdfm" }
+           };
+            Model model = new Model() { name = "code", age = 90, salary = 89.09 };
+
+            HttpWebRequestHelper<List<Model>>.Instance
+                  .OnStart(() =>
+                  {
+                      Console.WriteLine("-------------STart ...");
+                  })
+                  .OnSuccess((result) =>
+                  {
+                      Console.WriteLine("Success ...");
+                      List<Model> models = result.Data;
+                      for (int i = 0; i < models.Count; i++)
+                      {
+                          Model m = models[i];
+                          Console.WriteLine("name:" + m.name + " age :" + m.age + " salary:" + m.salary);
+                      }
+                  })
+                  .OnFailure((res) =>
+                  {
+                      Console.WriteLine("Failure ..code:+" + res.Code + "...msc:" + res.Message);
+                  })
+                  .OnError((res) =>
+                  {
+                      MessageBox.Show(res.Code + " : " + res.Message);
+                      Console.WriteLine("Error ..code:+" + res.Code + "...msc:" + res.Message);
+                  })
+                  .SetHeaders(headerCollection)
+                 // .Post(url, collection);
+                 .Post<Model>(url, model);
+        }
+
+
+        public void TestGet()
+        {
+            string url = "http://127.0.0.1:8080/nbm/login/test";
+
+            HttpWebRequestHelper<List<Model>>.Instance
+                .OnStart(() =>
+                {
+                    Console.WriteLine("STart ...");
+                })
+                .OnSuccess((result) =>
+                {
+                    Console.WriteLine("Success ...");
+                    List<Model> models = result.Data;
+                    for (int i = 0; i < models.Count; i++)
+                    {
+                        Model m = models[i];
+                        Console.WriteLine("name:" + m.name + " age :" + m.age);
+                    }
+                })
+                .OnFailure((res) =>
+                {
+                    Console.WriteLine("Failure ..code:+" + res.Code + "...msc:" + res.Message);
+                })
+                .OnError((res) =>
+                {
+                    MessageBox.Show(res.Code + " : " + res.Message);
+                    Console.WriteLine("Error ..code:+" + res.Code + "...msc:" + res.Message);
+                })
+                .Get(url, "name=code&age=56");
         }
     }
 }
